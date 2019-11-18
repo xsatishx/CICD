cf69cb5823a5f3d36fc280699c2ee9fdc9d20d58
Balakrishnan <satishb@hpe.com>
2019-11-18 14:25:11 +0800

    Added parameters

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,7 @@ provider "aws" {
 module "ec2-instance" {
   //https://registry.terraform.io/modules/terraform-aws-modules/ec2-instance/aws/1.19.0
   source                 = "github.com/xsatishx/terraform/Modules/EC2"
-  user_data              = "${file("installChef.sh")}"
+  user_data              = "apt-get update && apt-get install git && curl -L https://www.opscode.com/chef/install.sh | bash"
 
   }
   
