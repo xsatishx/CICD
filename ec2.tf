ff9f613e32abd5b46ff83185b102a17282e45073
Balakrishnan <satishb@hpe.com>
2019-11-19 10:51:40 +0800

    updated tf file

--- ec2.tf
+++ ec2.tf
@@ -6,5 +6,6 @@ module "ec2-instance" {
   //https://registry.terraform.io/modules/terraform-aws-modules/ec2-instance/aws/1.19.0
   source                 = "github.com/xsatishx/terraform/Modules/EC2"
   ami 					 = "ami-02c27cec4ad9d38ca"
-  }
+  user_data	             = "cd /home/ubuntu/ && git clone https://github.com/xsatishx/CICD.git"
+}
   
